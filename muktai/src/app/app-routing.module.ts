b7532520c082e0ad6b28f6aba3171a903ea06687
Rajesh Sonawane <annuraj261998@gmail.com>
2022-06-28 10:17:36 +0530

    about page designed

--- muktai/src/app/app-routing.module.ts
+++ muktai/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+  path:'', component: MainPageComponent },
+{ path: 'contact', component: ContactComponent },
+{ path: 'about', component: AboutUsComponent }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
